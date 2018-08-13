c08fc3d64ce9ede1f2ff2e5413bda2e419f465e8
geisonmsilva12 <geisonmsilva12@gmail.com>
2018-08-13 16:48:35 -0300

    estrutura spring criada

---
+++ src/main/java/com/engsoftware/r2d2/R2d2JavaApplication.java
@@ -0,0 +1,12 @@
+package com.engsoftware.r2d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2d2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2d2JavaApplication.class, args);
+	}
+}
